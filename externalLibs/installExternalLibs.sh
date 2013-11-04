35b51c7f367f1c2b5121e021258f24ccda398896
R. Matt McCann <mccann.matt@gmail.com>
2013-11-04 16:38:35 -0500

    Temporarily added external libs directory until the project can be refactored

---
+++ externalLibs/installExternalLibs.sh
@@ -0,0 +1,5 @@
+mvn install:install-file -Dfile=slf4j-android-1.6.1-CUSTOM.jar \
+                         -DgroupId=org.slf4j \
+                         -DartifactId=slf4j-android \
+                         -Dversion=1.6.1-CUSTOM \
+                         -Dpackaging=jar
